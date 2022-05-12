bc7b18e612b938616e2f3f72d883fb7b6437d1ea
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-12 11:57:13 +0530

    yes me

--- Array/ArrayPrototype.js
+++ Array/ArrayPrototype.js
@@ -11,8 +11,8 @@
 // fruits.myUcase();
 //It is use create methods for array objects
 Array.prototype.double = function() {
-    for (obj in this) {
-        obj = obj * 2;
+    for (let i = 0; i < this.length; i++) {
+        this[i] = this[i] * 2;
     }
 }
 var num = [23, 4, 325, 6, 5, 634];
