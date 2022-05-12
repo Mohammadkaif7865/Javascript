a85b183bbfe6b9e81544eacdcce1627e34b4228e
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-12 08:36:25 +0530

    yes me

--- Array/find.js
+++ Array/find.js
@@ -5,4 +5,13 @@ let x = ages.find(function(age, index, arr) {
 });
 //Any How the find function could not return index it can use index and array but cannot return the index or complete array
 // array.find(function(currentValue, index, arr),thisValue)
+// The find() method returns the value of the first element that passes a test.
+
+// The find() method executes a function for each array element.
+
+// The find() method returns undefined if no elements are found.
+
+// The find() method does not execute the function for empty elements.
+
+// The find() method does not change the original array.
 console.log(x);
---
+++ Array/findIndex.js
@@ -0,0 +1,12 @@
+let x = [234, 25, 34, 645, 7, 547, 6, 85, 78, 56, 753, 634, 5, 324, 3, 234];
+let y = x.findIndex(currVal => currVal == 5);
+console.log(y);
+//In arrow function no need to give parenthesis if only one parameter is There
+//No need to give curly braces if one line code in function and not even return keyword is require
+// const ages = [3, 10, 18, 20];
+
+// ages.findIndex(checkAge);
+
+// function checkAge(age) {
+//   return age > 18;
+// }
