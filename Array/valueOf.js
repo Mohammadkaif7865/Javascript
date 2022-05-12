fcb7aea62c3bed4cdaabf9aaf8cc3140f2e32b10
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-12 13:41:41 +0530

    yes me

---
+++ Array/valueOf.js
@@ -0,0 +1,13 @@
+// Definition and Usage
+// The valueOf() method returns the array itself.
+
+// The valueOf() method does not change the original array.
+
+// array.valueOf() returns the same as array.
+// Return Value
+// Type	Description
+// An array	The valueOf() method returns itself!
+// array.valueOf()
+let x = [34, "Monday", "Toss", 34, 534];
+let y = x.valueOf(); //return itself or x
+console.log(x, y);
