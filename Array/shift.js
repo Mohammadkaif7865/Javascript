40273d3fe0e015014782e90301f27fa97cf98749
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-12 12:38:23 +0530

    yes me

---
+++ Array/shift.js
@@ -0,0 +1,9 @@
+// Definition and Usage
+// The shift() method removes the first item of an array.
+
+// The shift() method changes the original array.
+
+// The shift() method returns the shifted element.
+let x = [234, 45, 34, 654, 7, 678, 6, 5, 4, 3];
+let y = x.shift();
+console.log(x, y);
