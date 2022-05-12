a431b2b92e3080b899f84262c0d9d3afcd04d807
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-12 12:33:31 +0530

    yes me

---
+++ Array/reverse.js
@@ -0,0 +1,7 @@
+// Definition and Usage
+// The reverse() method reverses the order of the elements in an array.
+
+// The reverse() method overwrites the original array.
+let x = [3432, 534, 6, 56, 547, 645, 7665, 76, 7];
+let y = x.reverse();
+console.log(x, y);
