aeeacd3454322093dcb3216c6ff36c69cd4c1678
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-12 12:45:31 +0530

    yes me

---
+++ Array/slice.js
@@ -0,0 +1,10 @@
+// array.slice(start, end)
+// Definition and Usage
+// The slice() method returns selected elements in an array, as a new array.
+
+// The slice() method selects from a given start, up to a (not inclusive) given end.
+
+// The slice() method does not change the original array.
+let x = [353, 53, 46, 45, 654, 7, 67, 54, 6, 4];
+let y = x.slice(2, 8);
+console.log(x, y);
