b8ac1e931512ab8f9045f940b5d05f4c23f8dffc
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-13 09:12:18 +0530

    yes me

---
+++ String/charAt.js
@@ -0,0 +1,12 @@
+// Definition and Usage
+// The charAt() method returns the character at a specified index (position) in a string.
+
+// The index of the first character is 0, the second 1, ...
+
+// The index of the last character is string length - 1 (See Examples below).
+
+// See also the charCodeAt() method.
+let str = "I am the trade boy";
+console.log(str.charAt(5));
+//Output
+// t
