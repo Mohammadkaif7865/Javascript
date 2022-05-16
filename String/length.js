b4fedaf6054d0f973b342aa798a778198954285f
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-16 12:47:02 +0530

    yes em

---
+++ String/length.js
@@ -0,0 +1,9 @@
+// Definition and Usage
+// The length property returns the length of a string.
+
+// The length property of an empty string is 0.
+// string.length
+let x = "Hey there! Do you remember me , if not try to recall your days when you were in third class who was your best friend.";
+let length = x.length;
+//Output : 117
+console.log(length);
