406f591f6add1bdfbaf912e53d86be6cfd582523
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-16 12:18:45 +0530

    yes me

---
+++ String/fromCharCode.js
@@ -0,0 +1,19 @@
+// Definition and Usage
+// The String.fromCharCode() method converts Unicode values to characters.
+
+// The String.fromCharCode() is a static method of the String object.
+
+// The syntax is always String.fromCharCode().
+
+// You cannot use myString.fromCharCode().
+// let x = String.fromCharCode(0);
+// let x = String.fromCharCode(1);
+// Output : 
+// let x = String.fromCharCode(11);
+// Output :
+// let x = String.fromCharCode(1, 11, 21, 65, 34);
+// Output : A"
+// let x = String.fromCharCode(65, 54);
+// Output : A6
+
+console.log(x);
