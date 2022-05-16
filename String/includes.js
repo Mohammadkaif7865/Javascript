be2d34729ec590185b88f5491e3072b65ddc3562
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-16 12:27:57 +0530

    yes me

---
+++ String/includes.js
@@ -0,0 +1,15 @@
+// Definition and Usage
+// The includes() method returns true if a string contains a specified string.
+
+// Otherwise it returns false.
+
+// The includes() method is case sensitive.
+// string.includes(searchvalue, start)
+let str = "My name is Mohammad kaif, and i don't know the meaning of my name";
+// let res = str.includes("Know", 0);
+//Output : false because the includes method is case sensitive
+// let res = str.includes("know", 0);
+// Output : true  because the includes method is case sensitive
+// let res = str.includes("know", 60);
+// Output : false because the includes method start point index is 60
+console.log(res);
