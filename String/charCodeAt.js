0ddb4564689711fd0e3adb625874eb128d9ed215
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-16 11:39:28 +0530

    yes me

---
+++ String/charCodeAt.js
@@ -0,0 +1,9 @@
+// The charCodeAt() method returns the Unicode of the character at a specified index (position) in a string.
+
+// The index of the first character is 0, the second is 1, ....
+// string.charCodeAt(index)
+let str = "My name is Kaif";
+// console.log(str.charCodeAt(6));
+//Output : 101
+// console.log(str.charAt(6));
+//Output : e
