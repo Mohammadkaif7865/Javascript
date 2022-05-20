3e417a0353da7741c01a872592f136958e6c7a2d
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 10:58:12 +0530

    ye me

---
+++ Boolean/valueOf.js
@@ -0,0 +1,13 @@
+// Definition and Usage
+// valueOf() returns the primitive value of a boolean.
+
+// valueOf() is usually called by JavaScript behind the scenes, and not explicitly in code.
+
+// Syntax
+// boolean.valueOf()
+// Return Value
+// Type	Description
+// A boolean	true or false.
+let x = true;
+let y = x.valueOf();
+console.log(y);
