1eda2210d596a6c0e24081b964d7b787cd150e53
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 09:26:27 +0530

    ys me

---
+++ String/valueOf.js
@@ -0,0 +1,17 @@
+// Definition and Usage
+// The valueOf() method returns the primitive value of a string.
+
+// The valueOf() method does not change the original string.
+
+// The valueOf() method can be used to convert a string object into a string.
+// Syntax
+// string.valueOf()
+// Parameters
+// NONE
+// Return Value
+// Type	Description
+// A string	The primitive value of the string.
+let str = "Here we go...";
+let str2 = str.valueOf();
+console.log(str2);
+//Here we go...
