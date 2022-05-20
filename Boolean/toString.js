cda5b14faa077bb4db77dda24b237e11cf660e49
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 10:55:47 +0530

    yes me

---
+++ Boolean/toString.js
@@ -0,0 +1,20 @@
+// Definition and Usage
+// The toString() method returns a boolean as a string.
+
+// Note
+// Every JavaScript object has a toString() method.
+
+// The toString() method is used internally by JavaScript when an object needs to be displayed as a text (like in HTML), or when an object needs to be used as a string.
+
+// Normally, you will not use it in your own code.
+
+// Syntax
+// boolean.toString()
+// Parameters
+// NONE
+// Return Value
+// Type	Description
+// A string	"true" or "false".
+let x = true;
+let y = x.toString();
+console.log(y);
