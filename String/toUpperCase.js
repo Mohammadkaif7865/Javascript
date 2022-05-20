9ddc6bdb164623d680f97cc4c3001b70737f84ec
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 09:14:41 +0530

    yes me

---
+++ String/toUpperCase.js
@@ -0,0 +1,15 @@
+// Definition and Usage
+// The toUpperCase() method converts a string to uppercase letters.
+
+// The toUpperCase() method does not change the original string.
+// Syntax
+// string.toUpperCase()
+// Parameters
+// NONE
+// Return Value
+// Type	Description
+// A string	The string converted to uppercase.
+let str = "Hi how are you";
+let All_upper = str.toUpperCase();
+console.log(All_upper);
+//HI HOW ARE YOU
