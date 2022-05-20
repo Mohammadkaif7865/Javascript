167b98657cff41c62a687bb3181b745416bdbd9f
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 09:20:16 +0530

    yes me:

---
+++ String/toString.js
@@ -0,0 +1,22 @@
+// Definition and Usage
+// The toString() method returns a string as a string.
+
+// The toString() method does not change the original string.
+
+// The toString() method can be used to convert a string object into a string.
+// Syntax
+// string.toString()
+// Parameters
+// NONE
+// Return Value
+// Type	Description
+// A string	The content of the string.
+let str = [3, 4, 5, 56, 547, 568, 34];
+console.log(str);
+//[
+//     3,   4,  5, 56,
+//   547, 568, 34
+// ]
+
+// console.log(str.toString());
+//3,4,5,56,547,568,34
