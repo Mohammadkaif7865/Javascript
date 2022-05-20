757466f034ce373641c5ed842af300fbc9247418
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 09:16:41 +0530

    yes me

---
+++ String/toLowerCase.js
@@ -0,0 +1,15 @@
+// Definition and Usage
+// The toLowerCase() method converts a string to lowercase letters.
+
+// The toLowerCase() method does not change the original string.
+// Syntax
+// string.toLowerCase()
+// Parameters
+// NONE
+// Return Value
+// Type	Description
+// A string	The string converted to lowercase.
+let str = "Hello how are you?";
+let toLowerCase = str.toLowerCase();
+console.log(toLowerCase);
+//hello how are you?
