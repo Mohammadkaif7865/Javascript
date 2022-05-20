d5e8aa16f1c0797e050c33be646d593c84c18604
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 08:31:59 +0530

    yes me

---
+++ String/search.js
@@ -0,0 +1,25 @@
+// Definition and Usage
+// The search() method matches a string against a regular expression **
+
+// The search() method returns the index (position) of the first match.
+
+// The search() method returns -1 if no match is found.
+
+// The search() method is case sensitive.
+// Note
+// ** If the search value is a string, it is converted to a regular expression.
+// Syntax
+// string.search(searchValue)
+// Parameters
+// Parameter	Description
+// searchValue	Required.
+// The search value.
+// A regular expression (or a string that will be converted to a regular expression).
+// Return Value
+// Type	Description
+// A number	The position of the first match.
+// -1 if no match.
+let str = "let me tell you that your And passion is not worthy for every one and if so it will not effect any one so choose you passion wisely and be worthy";
+// let count = str.search("and");
+// let count = str.search(/and/i);
+console.log(count);
