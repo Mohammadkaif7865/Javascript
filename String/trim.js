6d068c5ad1d88fb94afc2ff114051de08a78c066
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-20 09:22:56 +0530

    yees me

---
+++ String/trim.js
@@ -0,0 +1,15 @@
+// Definition and Usage
+// The trim() method removes whitespace from both sides of a string.
+
+// The trim() method does not change the original string.
+
+// Syntax
+// string.trim()
+// Parameters
+// NONE
+// Return Value
+// Type	Description
+// A string	A string with removed whitespace from both ends.
+let str = "   Hello World !!!   ";
+console.log(str.trim());
+//Hello World !!!    But it cannot trim the middle of a string whitespace
